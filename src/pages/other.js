ac428c5c245a260b5c9483a3ccff1e9ea0e09442
Kevin Förster <kevin.foerster@sapient.com>
2020-12-09 09:33:01 +0100

    actual code

--- src/pages/other.js
+++ src/pages/other.js
@@ -5,14 +5,11 @@ import { navigate } from 'gatsby'
 
 
 const Index = (props) => {
-  // default color if no color is in state
-  const currentColor = props?.location?.state?.color || '#4e8d7c'
-  // calculate nextColor from currentColor
-  const nextColor = Color(currentColor).rotate(50).string()
 
+  const currentColor = props?.location?.state?.color || '#4e8d7c'
+  const nextColor = Color(currentColor).rotate(50).string()
   const [color, setColor] = useState(currentColor)
 
-  // setup timeout to change color after component has been mounted
   useEffect(() => {
       const timer  = setTimeout(() => setColor(nextColor), 1000)
       return () => {
@@ -21,9 +18,9 @@ const Index = (props) => {
   }, [])
 
   return (
-    <Flex sx={{ width: '100vw', height: '100vh', backgroundColor: color , justifyContent: 'center', alignItems: 'center', transition: 'all ease-in 0.2s'}}>
+    <Flex sx={{ width: '100vw', height: '100vh', backgroundColor: color , justifyContent: 'center', alignItems: 'center', transition: 'all ease-in 2s'}}>
       <Box sx={{border: '3px solid black', p: 2, cursor: 'pointer'}} onClick={() => {
-        navigate('/other', {state: {color: color}})
+        navigate('/', {state: {color: color}})
       }}>
         random {color}
       </Box>
